1e8f06618d5cd59a42526e85ca33d6770d2336ed
Gokul-J <jgokul2000@gmail.com>
2020-09-28 08:42:07 +0530

    Updated

--- Main.java
+++ Main.java
@@ -3,23 +3,45 @@ import java.util.Scanner;
 public class Main {
   private static Scanner sc = new Scanner(System.in);
   public static void main(String[] args){
-    System.out.println("Enter your Sequence to solve :");
+    System.out.println("Enter your Expression to solve :");
     String s = sc.nextLine();
-    newProb(s);
+    valid(s);
+  }
+
+  public static void valid(String s){
+    boolean valid = s.matches("^[0-9()*+-/\\s]+");
+    if(valid){
+      newProb(s);
+    }
+    else{
+      System.out.println("Invalid Expression. Please enter a Valid Expression.");
+      newProb("");
+    }
   }
 
   public static void newProb(String s){
-    Calculator calc = new Calculator();
-    calc.eval(s);
-    System.out.println("Have another Sequence ? Y or N");
-    char c = sc.nextLine().trim().toLowerCase().charAt(0);
+    if(s!=""){
+      Calculator calc = new Calculator();
+      calc.eval(s);
+    }
+    System.out.println("Have another Expression ? Y or N");
+    String cont = sc.nextLine().trim().toLowerCase();
+    if(cont.length()>1){
+      System.out.println("Invalid Input");
+      newProb("");
+    }
+    char c = cont.charAt(0);
     if(c=='y'){
-      System.out.println("Enter your Sequence to solve :");
+      System.out.println("Enter your Expression to solve :");
       String in = sc.nextLine();
-      newProb(in);
+      valid(in);
+    }
+    else if(c=='n'){
+      System.out.println("Thank You...");
     }
     else{
-      System.out.println("Thank You...");
+      System.out.println("Invalid Input");
+      newProb("");
     }
   }
 }
